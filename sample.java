0871ccc38605a23ca52c6219c1a2e74f4dbb857a
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-03-28 19:26:04 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -8,3 +8,20 @@ class Solution {
         return single;
     }
 }
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor=0;
+        for (int num:nums) {
+            xor = xor^num;
+        }
+        int twoscomp = xor & (-xor);
+        int xor2=0;
+        for (int num:nums) {
+            if ((num & twoscomp) != 0 ){
+                xor2 = xor2^num;
+            }
+        }
+
+        return new int[]{xor2, xor^xor2};
+    }
+}
